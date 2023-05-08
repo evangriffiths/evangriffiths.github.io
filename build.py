8da0ddb4f0774e8622fac2cf2344455f6792c702
evangriffiths <egriffiths93@gmail.com>
2023-05-08 16:53:14 +0100

    Add jinja2 templating for header and footer

---
+++ build.py
@@ -0,0 +1,19 @@
+import jinja2 as jj
+import os
+import glob
+
+os.system("rm -rf build")
+os.system("mkdir build")
+os.chdir("src")
+
+# Create a Jinja2 environment
+env = jj.Environment(loader=jj.FileSystemLoader("."))
+
+# Render and save HTML files
+for f in glob.glob("site/*", recursive=True):
+    template = env.get_template(f)
+    output = template.render()
+
+    output_path = f.replace("site", "../build")
+    with open(output_path, "w") as f:
+        f.write(output)
